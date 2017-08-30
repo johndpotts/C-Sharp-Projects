d25a3ddc01629ffe48a62c04f3035e09ea952cb3
John Potts <john@arlingtonchurch.com>
2017-08-30 16:34:11 -0400

    fixed files

---
+++ HelloWorld/Hello.cs
@@ -0,0 +1,17 @@
+﻿//My very first C# program! 
+
+using System;
+
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            // Keep the console open in debug mode }
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
